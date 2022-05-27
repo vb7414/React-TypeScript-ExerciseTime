59fc01e69cc721285626843a8159a00450c9e55a
FA00405\luke.chen <luke.chen@shoalter.com>
2022-05-27 14:45:58 +0800

    exercise time

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -3,7 +3,6 @@ import Todo from "../models/todo";
 import TodoItem from "./TodoItem";
 
 //FC functional component, describes a type defined by the React package, is generic
-//Todop[]
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
     return (
         <ul>
@@ -11,8 +10,6 @@ const Todos: React.FC<{ items: Todo[] }> = (props) => {
                 //bcz React.FC, can add key prop here
                 <TodoItem key={item.id} text={item.text} />
             ))}
-            {/* <li>learn react</li>
-            <li>learn typescript</li> */}
         </ul>
     );
 };

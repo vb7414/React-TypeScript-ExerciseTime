c4c01b25dcb81c5e1193d8a50233fa83dd2b34cf
FA00405\luke.chen <luke.chen@shoalter.com>
2022-05-30 09:36:31 +0800

    use filter to remove the todo

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -4,12 +4,19 @@ import TodoItem from "./TodoItem";
 import classes from "./Todos.module.css";
 
 //FC functional component, describes a type defined by the React package, is generic
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (
+    props
+) => {
     return (
         <ul className={classes.todos}>
             {props.items.map((item) => (
                 //bcz React.FC, can add key prop here
-                <TodoItem key={item.id} text={item.text} />
+                <TodoItem
+                    key={item.id}
+                    text={item.text}
+                    //bind allows to pre-configure a function for future execution
+                    onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+                />
             ))}
         </ul>
     );
